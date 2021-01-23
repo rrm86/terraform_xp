2bd076cb461c2a9c4465321f3fa787009b70ac13
Ronnald R Machado <rrmachado@id.uff.br>
2021-01-23 13:32:24 -0300

    Hardcoded keys removed

--- vars.tf
+++ vars.tf
@@ -10,20 +10,17 @@ variable "location" {
 }
 variable "subscription_id" {
     description = "The Azure subscription_id"
-    default = "691473ea-84fe-4297-ada8-38bd99eeb456"
-  
+
 }
 variable "client_id" {
     description = "The Azure client_id"
-    default = "540fe01d-caf6-4493-9118-8aa03b43e09e"
-  
+
 }
 variable "client_secret" {
     description = "The Azure client_secret"
-  
+
 }
 variable "tenant_id" {
     description = "The Azure tenant_id"
-    default = "48558b3c-26f0-4e76-90e5-1ec85c2308d3"
-  
+    
 }
